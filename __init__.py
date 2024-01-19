9bf090d2204c72d1943daa9a524022fa97f9635f
MS-TWHOMCVJZCUH\Administrator <455758525@qq.com>
2024-01-19 20:21:48 +0800

    删除GPT节点

--- __init__.py
+++ __init__.py
@@ -1,19 +1,16 @@
 # from .mine_nodes import *
-from .CXH_GPT import *
 from .CXH_ALY_Seg_Cloth import *
 from .CXH_IMAGE import *
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
-    "CXH_GPT":CXH_GPT,
     "CXH_ALY_Seg_Cloth":CXH_ALY_Seg_Cloth,
     "CXH_IMAGE":CXH_IMAGE
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "CXH_GPT": "CXH_GPT",
     "CXH_ALY_Seg_Cloth":"CXH_ALY_Seg_Cloth",
     "CXH_IMAGE":"CXH_IMAGE"
 }
